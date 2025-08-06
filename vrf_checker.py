8adc1eb023d5b1d9a165cfbf5340bb438cc139ca
Abstract Potato <21150449+abstractpotato@users.noreply.github.com>
2025-08-06 19:41:55 -0400

    Update vrf_checker.py

--- vrf_checker.py
+++ vrf_checker.py
@@ -2,8 +2,6 @@ import random
 from hashlib import sha256
 from json import loads
 
-filename = str(input("enter snapshot filename: "))
-print(filename)
-file_text = open(filename).read()
+file_text = open(str(input("enter snapshot filename: "))).read()
 random.seed(file_text)
 print(random.choice(loads(file_text))["stake_addr"])
